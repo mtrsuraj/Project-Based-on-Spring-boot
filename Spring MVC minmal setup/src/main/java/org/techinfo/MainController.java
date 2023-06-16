c11f964f5f3216aeac69bdf79b63526dcfa8c038
mtrsuraj <surajprintf33@gmail.com>
2023-06-16 23:30:04 +0530

    Spring setup

---
+++ Spring MVC minmal setup/src/main/java/org/techinfo/MainController.java
@@ -0,0 +1,22 @@
+package org.techinfo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String welcome() {
+		return "hello";
+	}
+	@RequestMapping("displayname")
+	public String displayName(HttpServletRequest request) {
+		String firstName=request.getParameter("firstName");
+		request.setAttribute("firstName", firstName);
+		return "displayName";
+	}
+}
